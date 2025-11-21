8cf41fe54d745201e7c12796136575345d9571a8
Darichel <darichel.perez@gmail.com>
2025-11-21 12:42:11 -0300

    feat/column tag with validation

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -28,6 +34,12 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  @Column('text', {
+    array: true,
+    default: [],
+  })
+  tags: string[];
 
   @BeforeInsert()
   checkSlugInsert() {
