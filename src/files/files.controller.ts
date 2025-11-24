88454cbbfea680955cfcebb34a5614e32afedddd
Darichel Perez <darichel.perez@gmail.com>
2025-11-24 12:04:30 -0300

    feat/serve files in a controller way

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,28 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import type { Response } from 'express';
 import { fileFileter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:id')
+  findProductImage(@Res() res: Response, @Param('id') id: string) {
+    const path = this.filesService.getStaticProductImage(id);
+    res.sendFile(path);
+  }
 
   @Post()
   @UseInterceptors(
@@ -21,15 +31,14 @@ export class FilesController {
       storage: diskStorage({
         destination: './static/products',
         filename: fileNamer,
-      })
+      }),
     }),
   ) //Interceptor ,'file' must match the field name in the form-data
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('File not provided');
 
-    return {
-      fileName: file.originalname,
-      type: file.mimetype,
-    };
+    const secureUrl = `${file.filename}`;
+
+    return { secureUrl };
   }
 }
