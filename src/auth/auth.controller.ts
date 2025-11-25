b5ed2f4e95e703d1beb2baa6d1c29d69708d73ce
Darichel Perez <darichel.perez@gmail.com>
2025-11-25 15:17:10 -0300

    feat/custom guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto, LoginUserDto } from './dto'; 
+import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from './decorators/raw-headers.decorator';
-
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -24,18 +24,30 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
-   @Req() request: Express.Request,
-   @GetUser() user: User,
-   @GetUser('email') userEmail: string,
-   @RawHeaders() rawHeaders: string[], // This decorator will be in common module
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[], // This decorator will be in common module
   ) {
-    console.log({ rawHeaders });
+    //console.log({ rawHeaders });
     return {
       ok: true,
       message: 'Hola mundo private',
       user,
       userEmail,
       rawHeaders,
+    };
+  }
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(
+    @RawHeaders() rawHeaders: string[], // This decorator will be in common module
+  ) {
+    //console.log({ rawHeaders });
+    return {
+      ok: true,
+      message: 'Hola mundo private',
     };
   }
 }
