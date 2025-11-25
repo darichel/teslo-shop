85d5d1436b6aee18f16e01557ea8093af40174bf
Darichel Perez <darichel.perez@gmail.com>
2025-11-25 11:01:06 -0300

    feat/login user, bcrypt library

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
+
+import * as bcrypt from 'bcrypt';
+
 import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -13,12 +16,40 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
       await this.userRepository.save(user);
+      user.password = '******';
       return user;
+      // TODO: JWT for authentication
     } catch (error) {
       this.handleDbErros(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+     });
+    
+    if (!user) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials - password');
+    }
+    
+    return user;
+    // TODO: JWT for authentication
   }
 
   private handleDbErros(error: any): never {
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -8,7 +8,7 @@ export class User {
   @Column('text', { unique: true })
   email: string;
 
-  @Column('text')
+  @Column('text', { select: false }) // select: false to exclude password from queries by default
   password: string;
 
   @Column('text')
