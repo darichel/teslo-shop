bbd1d5f5959ecd8029a9a1f69e832c548d185390
Darichel <darichel.perez@gmail.com>
2025-11-26 14:47:50 -0300

    feat/Decorator compositions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post, Body, Get, UseGuards, Req, SetMetadata } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
-import { RawHeaders } from './decorators/raw-headers.decorator';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
-import { RoleProtected } from './decorators/role-protected.decorator';
 import { ValidRoles } from './interfaces/valid-roles.interface';
+import { GetUser, RawHeaders, RoleProtected, Auth} from './decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -40,6 +46,7 @@ export class AuthController {
       rawHeaders,
     };
   }
+
   @Get('private2')
   //@SetMetadata('roles', ['admin', 'super-user'])
   @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
@@ -51,6 +58,20 @@ export class AuthController {
     return {
       ok: true,
       message: 'Hola mundo private',
+    };
+  }
+
+  @Get('private3')
+  //@SetMetadata('roles', ['admin', 'super-user'])
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
+  testingPrivateRoute3(
+    @GetUser() user: User,
+  ) {
+    //console.log({ rawHeaders });
+    return {
+      ok: true,
+      message: `Hola ${user.fullName}`,
+      user,
     };
   }
 }
