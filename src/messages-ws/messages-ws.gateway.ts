0188e18fe77d5a6dc39a1fa0acade09b101e1fe3
Darichel Perez <darichel.perez@gmail.com>
2025-12-01 09:56:45 -0300

    feat/Sockets - clients conected

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,22 +1,25 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
   
   handleConnection(client: Socket) {
     //console.log('Client connected', client.id);
     this.messagesWsService.conectedClient(client);
-    console.log(this.messagesWsService.countConnectedClients());
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     // console.log('Client disconnected', client.id);
     this.messagesWsService.disconnectClient(client.id);
-    console.log(this.messagesWsService.countConnectedClients());
-
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
 
